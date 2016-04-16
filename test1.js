3028e53da90836113719a798b2ba78abe8b7675f
PaulineOC <poc215@nyu.edu>
2016-04-16 17:25:15 -0400

    Create test1.js
    Pauline's functions to glean which ROW to color

---
+++ test1.js
@@ -0,0 +1,45 @@
+
+
+var all_times = new Array(40);
+var old_date = new Date();
+old_date.setHours(8)
+old_date.setMinutes(00);
+
+
+
+for(i=0;i<39;i++){//goes through for loop and creates a map of times
+	var new_date= new Date((old_date).getTime() + (15 * 6000))
+	var num = newDateobj.valueOf()
+	all_times[i] = num;
+	old_date = new_date;
+}
+
+
+
+
+
+"ARTH-UA 450-001
+ (8048)" —————————>   TuTh      3:30PM    -    4:45PM"
+
+
+PER CLASS: 
+
+var time_chunk =  time_1.split(“ ”);
+var day  = time_chunk[0] 
+
+
+var start_time  =  time_chunk[1] 
+		var time_only = start_time.split(([1]?[1-9]:[0-9]{2})
+		start_time  = time_only[0]
+
+var end_time =  time_chunk[3]
+	time_only = end_time.split(([1]?[1-9]:[0-9]{2})
+	end_time = time_only[0]
+
+
+
+//to actually color:
+
+
+
+
