d4437eea63dce22aebd9144b490d651e9f2c7950
PaulineOC <poc215@nyu.edu>
2016-04-16 18:12:33 -0400

    split time string

--- test1.js
+++ test1.js
@@ -1,44 +1,46 @@
 
+// "ARTH-UA 450-001
+//  (8048)" —————————>   TuTh      3:30PM    -    4:45PM"
+//NEED TO DO PER  CLASS object
 
-var all_times = new Array(40);
-var old_date = new Date();
-old_date.setHours(8)
-old_date.setMinutes(00);
-
-
-
-for(i=0;i<39;i++){//goes through for loop and creates a map of times
-	var new_date= new Date((old_date).getTime() + (15 * 6000))
-	var num = newDateobj.valueOf()
-	all_times[i] = num;
-	old_date = new_date;
-}
+var time_chunk =  time_1.split(" ");
 
 
-
-
-
-"ARTH-UA 450-001
- (8048)" —————————>   TuTh      3:30PM    -    4:45PM"
-
-
-PER CLASS: 
-
-var time_chunk =  time_1.split(“ ”);
 var day  = time_chunk[0] 
 
+var start_time  =  time_chunk[1]
+var time_only = start_time.slice(-2)
+start_time  = time_only[0]
 
-var start_time  =  time_chunk[1] 
-		var time_only = start_time.split(([1]?[1-9]:[0-9]{2})
-		start_time  = time_only[0]
 
-var end_time =  time_chunk[3]
-	time_only = end_time.split(([1]?[1-9]:[0-9]{2})
-	end_time = time_only[0]
 
+//will 
+
+var end_time  =  time_chunk[1]
+time_only = start_time.slice(-2)
+end_time = time_only[0]
+
+
+//CHECK INDICES IN ARRAYLIST
+
+var all_times = [
+"8:00","8:15","8:30","8:45",
+"9:00","9:15","9:30","9:45",
+"10:00","10:15","10:30","10:45",
+"11:00","11:15","11:30","11:45",
+"12:00","12:15","12:30","12:45",
+"1:00","1:15","1:30","1:45",
+"2:00","2:15","2:30","2:45",
+"3:00","3:15","3:30","3:45",
+"4:00","8:15","8:30","8:45",
+"5:00","8:15","8:30","8:45",
+"6:00"]
+
+//Calculate difference
 
 
 //to actually color:
+
 
 
 
