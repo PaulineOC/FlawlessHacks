16c103d4f6901915cf1d0ba681762e4190577982
Katy Herrick <kmh530@nyu.edu>
2016-04-16 13:23:59 -0400

    whoops sorry friends

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ head.appendChild(script)
 // Get course names
 var iframe = document.getElementsByTagName("frame")
 iframe = iframe[1] //wants to get the second iframe only
-var class_name_1 = iframe.contentWindow.getElementById("P_CLASS_NAME$#0").text
-var class_name_2 = iframe.contentWindow.getElementById("P_CLASS_NAME$#1").text
-var class_name_3 = iframe.contentWindow.getElementById("P_CLASS_NAME$#2").text
-var class_name_4 = iframe.contentWindow.getElementById("P_CLASS_NAME$#3").text
+var class_name_1 = iframe.contentWindow.document.getElementById("P_CLASS_NAME$#0").text
+var class_name_2 = iframe.contentWindow.document.getElementById("P_CLASS_NAME$#1").text
+var class_name_3 = iframe.contentWindow.document.getElementById("P_CLASS_NAME$#2").text
+var class_name_4 = iframe.contentWindow.document.getElementById("P_CLASS_NAME$#3").text
